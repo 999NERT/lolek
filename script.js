90fbfcd0ab973245016ff49555cc3cacf422e876
999NERT <999nert@gmail.com>
2025-09-08 05:35:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -93,15 +93,3 @@ tmobileModal.addEventListener('click', (e)=>{
     tmobileModal.classList.remove('show');
   }
 });
-
-// === ANIMACJA FADE-IN MINIATURKI + BORDER + TŁO ===
-document.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
-    document.querySelector('.latest-video-container').classList.add('visible');
-    document.getElementById('watchButton').classList.add('visible');
-  }, 2000);
-
-  loadLatestVideo();
-  checkStreamStatus();
-  setInterval(checkStreamStatus, 60000);
-});
